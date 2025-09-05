7613c804e0280f4b13630af4c2a98e9c64ec0193
nicholas-voigt <nicovo215@googlemail.com>
2025-09-05 15:48:02 +0800

    changed training shell script 2

--- Capstone_MVP_Test_Train.sh
+++ Capstone_MVP_Test_Train.sh
@@ -14,8 +14,8 @@
 #SBATCH --time=00-02:00:00          # How long to run the job for? Jobs exceed this time will be terminated
                                     # Format <DD-HH:MM:SS> eg. 5 days 05-00:00:00
                                     # Format <DD-HH:MM:SS> eg. 24 hours 1-00:00:00 or 24:00:00
-#SBATCH --mail-type=BEGIN,END,FAIL  # When should you receive an email?
-#SBATCH --output=logs/%u.%j.out        # Where should the log files go?
+#SBATCH --mail-type=END,FAIL        # When should you receive an email?
+#SBATCH --output=logs/%u.%j.out     # Where should the log files go?
                                     # You must provide an absolute path eg /common/home/module/username/
                                     # If no paths are provided, the output file will be placed in your current working directory
 #SBATCH --requeue                   # Remove if you do not want the workload scheduler to requeue your job after preemption
@@ -42,7 +42,7 @@ module load Python/3.13.1-GCCcore-13.3.0
 module load cuDNN/9.5.0.50-CUDA-12.6.0
 
 # Create a virtual environment can be commented off if you already have a virtual environment
-# python3 -m venv ~/Capstone
+python3 -m venv ~/Capstone
 
 # This command assumes that you've already created the environment previously
 # We're using an absolute path here. You may use a relative path, as long as SRUN is execute in the same working directory
