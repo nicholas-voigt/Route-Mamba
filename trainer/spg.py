da5171b8011d245fcd1106ee477aa092c86036cf
nicholas-voigt <nicovo215@googlemail.com>
2025-10-15 20:24:44 +0800

    revert back to reward in comparison to baseline & included grad_norm logging

--- trainer/spg.py
+++ trainer/spg.py
@@ -172,7 +172,7 @@ class SPGTrainer:
 
         ## Reward calculation using soft actions (tour distributions)
         actual_cost = compute_euclidean_tour(torch.bmm(discrete_actions.transpose(1, 2), observation))
-        reward = -actual_cost * self.opts.reward_scale  # Apply reward scaling
+        reward = (baseline_cost - actual_cost) * self.opts.reward_scale  # Apply reward scaling
 
         ## Add experience to replay buffer & log statistics
         replay_buffer.append(
