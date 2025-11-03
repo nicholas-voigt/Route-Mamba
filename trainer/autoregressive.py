040f202fd8b2d176c248f315771c4f037c6970ff
nicholas-voigt <nicovo215@googlemail.com>
2025-11-03 14:18:52 +0800

    removed log_likehood from loss functions for test

--- trainer/autoregressive.py
+++ trainer/autoregressive.py
@@ -1,13 +1,10 @@
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
-import torch.nn.functional as F
 import time
 from tqdm import tqdm
 
 from model.actor_network import ARPointerActor
-from model.critic_network import Critic
-from model import components as mc
 from utils.utils import compute_euclidean_tour, get_heuristic_tours
 from utils.logger import log_gradients
 
@@ -31,73 +28,20 @@ class ARTrainer:
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=opts.actor_lr)
         self.actor_scheduler = optim.lr_scheduler.LambdaLR(self.actor_optimizer, lambda epoch: opts.actor_lr_decay ** epoch)
 
-        # Initialize the critic with optimizer and learning rate scheduler
-        # if opts.critic_load_path:
-        #     print(f"Loading critic model from {opts.critic_load_path}")
-        #     self.critic = torch.load(opts.critic_load_path, map_location=opts.device)
-        # else:
-        #     self.critic = Critic(
-        #         input_dim = opts.input_dim,
-        #         embedding_dim = opts.embedding_dim,
-        #         mamba_hidden_dim = opts.mamba_hidden_dim,
-        #         mamba_layers = opts.mamba_layers,
-        #         dropout = opts.dropout,
-        #         mlp_ff_dim = opts.mlp_ff_dim,
-        #         mlp_embedding_dim = opts.mlp_embedding_dim
-        #     ).to(opts.device)
-        # self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=opts.critic_lr)
-        # self.critic_scheduler = optim.lr_scheduler.LambdaLR(self.critic_optimizer, lambda epoch: opts.critic_lr_decay ** epoch)
-
 
     def train(self):
         torch.set_grad_enabled(True)
         self.actor.train()
-        # if not self.opts.eval_only: self.critic.train()
 
 
     def eval(self):
         torch.set_grad_enabled(False)
         self.actor.eval()
-        # if not self.opts.eval_only: self.critic.eval()
 
 
     def start_training(self, problem):
         self.gradient_check = False
         
-        # --- Critic Warm-up Phase ---
-        # if self.opts.critic_warmup_epochs > 0:
-        #     print(f"\nCritic Warm-up for {self.opts.critic_warmup_epochs} epochs ---")
-        #     for epoch in range(self.opts.critic_warmup_epochs):
-        #         # prepare training dataset
-        #         train_dataset = problem.make_dataset(size=self.opts.graph_size, num_samples=self.opts.problem_size)
-        #         training_dataloader = DataLoader(dataset=train_dataset, batch_size=self.opts.batch_size)
-                
-        #         # Logging
-        #         print(f"\nCritic Warm-up Epoch {epoch}:")
-        #         print(f"-  Critic Learning Rate: {self.critic_optimizer.param_groups[0]['lr']:.6f}")
-                
-        #         logger = {
-        #             'critic_cost': [],
-        #             'critic_loss': []
-        #         }
-
-        #         # training batch loop
-        #         self.train()
-        #         start_time = time.time()
-
-        #         for _, batch in enumerate(tqdm(training_dataloader, disable=self.opts.no_progress_bar)):
-        #             self.train_batch(batch, logger, warmup_mode=True)
-
-        #         epoch_duration = time.time() - start_time
-        #         print(f"Critic Warm-up Epoch {epoch + 1} completed. Results:")
-        #         print(f"-  Epoch Runtime: {epoch_duration:.2f}s")
-        #         print(f"-  Average Critic Cost: {sum(logger['critic_cost'])/len(logger['critic_cost']):.4f}")
-        #         print(f"-  Average Critic Loss: {sum(logger['critic_loss'])/len(logger['critic_loss']):.4f}")
-        #         self.critic_scheduler.step()
-
-        #     print("\n--- Critic Warm-up Finished ---\n")
-
-        # --- Main Training Phase ---
         for epoch in range(self.opts.n_epochs):
             # prepare training dataset
             train_dataset = problem.make_dataset(size=self.opts.graph_size, num_samples=self.opts.problem_size)
@@ -106,13 +50,11 @@ class ARTrainer:
             # Logging
             print(f"\nTraining Epoch {epoch}:")
             print(f"-  Actor Learning Rate: {self.actor_optimizer.param_groups[0]['lr']:.6f}")
-            # print(f"-  Critic Learning Rate: {self.critic_optimizer.param_groups[0]['lr']:.6f}")
 
             logger = {
                 'critic_cost': [],
                 'actual_cost': [],
                 'actor_loss': [],
-                'critic_loss': [],
                 'advantage': [],
                 'log_likelihood': [],
                 'entropy': []
@@ -135,11 +77,9 @@ class ARTrainer:
             print(f"-  Average Advantage: {sum(logger['advantage'])/len(logger['advantage']):.4f}")
             print(f"-  Average Log Likelihood: {sum(logger['log_likelihood'])/len(logger['log_likelihood']):.4f}")
             print(f"-  Average Entropy: {sum(logger['entropy'])/len(logger['entropy']):.4f}")
-            # print(f"-  Average Critic Loss: {sum(logger['critic_loss'])/len(logger['critic_loss']):.4f}")
 
             # update learning rates
             self.actor_scheduler.step()
-            # self.critic_scheduler.step()
 
             # if (self.opts.checkpoint_epochs != 0 and epoch % self.opts.checkpoint_epochs == 0) or epoch == self.opts.n_epochs - 1:
             #     torch.save(self.actor, f"{self.opts.save_dir}/actor_{self.opts.problem}_epoch{epoch + 1}.pt")
@@ -163,30 +103,14 @@ class ARTrainer:
         baseline_tours = get_heuristic_tours(batch['coordinates'], self.opts.baseline_tours)
         estimated_cost = compute_euclidean_tour(baseline_tours)
 
-        # # Critic value estimation  & loss calculation using MSE loss
-        # estimated_cost = self.critic(observation).squeeze(1)
-        # critic_loss = F.mse_loss(estimated_cost, actual_cost.detach())
-
-        # Critic Logging
+        # Baseline Logging
         logger['critic_cost'].append(estimated_cost.mean().item())
-        # logger['critic_loss'].append(critic_loss.item())
-
-        # # Critic Update
-        # self.critic_optimizer.zero_grad()
-        # critic_loss.backward()
-        # torch.nn.utils.clip_grad_norm_(self.critic.parameters(), 1.0)
-        # if self.gradient_check:
-        #     log_gradients(self.critic)
-        # self.critic_optimizer.step()
-
-        # if warmup_mode:
-        #     return  # Skip actor update during warm-up phase
 
         # Actor loss using REINFORCE with critic baseline
         log_likelihood = torch.sum(log_probs, dim=1)  # (B,)
         entropy = torch.sum(entropies, dim=1)  # (B,)
-        advantage = ((actual_cost - estimated_cost) / estimated_cost).detach() * self.opts.reward_scale  # Apply reward scaling
-        actor_loss = (advantage * log_likelihood).mean() - 0.1 * entropy.mean()
+        advantage = ((actual_cost - estimated_cost) / estimated_cost) * self.opts.reward_scale  # Apply reward scaling
+        actor_loss = advantage.mean() - 0.1 * entropy.mean()
 
         # Logging
         logger['actual_cost'].append(actual_cost.mean().item())
