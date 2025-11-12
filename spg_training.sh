dccaf4cb6872f186b077270c930a8a2179956cba
nicholas-voigt <nicovo215@googlemail.com>
2025-11-12 12:31:47 +0800

    test changes in embedding on spg

--- spg_training.sh
+++ spg_training.sh
@@ -63,8 +63,8 @@ pip3 install -q --no-cache-dir --no-build-isolation mamba-ssm
 ## Parameters:
 TRAINER="spg"
 GRAPH_SIZE=20
-PROBLEM_SIZE=200000
-N_EPOCHS=25
+PROBLEM_SIZE=100000
+N_EPOCHS=10
 BATCH_SIZE=512
 BUFFER_SIZE=10000
 INITIAL_TOURS="polar"
