b8a4eebf8a072f271c49f9a7025bc030d578772c
nicholas-voigt <nicovo215@googlemail.com>
2025-11-13 20:55:14 +0800

    perm reinforce tsp50

--- spg_training.sh
+++ spg_training.sh
@@ -61,8 +61,8 @@ pip3 install -q --no-cache-dir --no-build-isolation mamba-ssm
 
 # Submit your job to the cluster
 ## Parameters:
-TRAINER="3po"
-GRAPH_SIZE=20
+TRAINER="spg"
+GRAPH_SIZE=50
 PROBLEM_SIZE=100000
 N_EPOCHS=40
 BATCH_SIZE=512
